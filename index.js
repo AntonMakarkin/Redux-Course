a1bc984b551130303ce4aae25c0aa1db49a6d5d2
AntonMakarkin <amakarkin27@gmail.com>
2023-02-04 16:25:51 +0300

    Part 7. Store

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const redux = require('redux');
+const createStore = redux.createStore
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 const orderCake = () => {
@@ -22,3 +24,14 @@ const reducer = (state = initialState, action) => {
             return state
     }
 }
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
