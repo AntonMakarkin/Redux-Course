fe43ebad2778770b53e65894cc807f6d055d196a
AntonMakarkin <amakarkin27@gmail.com>
2023-02-09 01:02:02 +0300

    part 23. extra reducers

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -3,14 +3,14 @@ const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
 const iceCreamReducer = require('../features/icecream/iceCreamSlice');
 
-const logger = reduxLogger.createLogger()
+//const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         iceCream: iceCreamReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 module.exports = store;
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -3,7 +3,9 @@ const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions;
 
 console.log('Initial state ', store.getState());
-const unsubscribe = store.subscribe(() => {});
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state ', store.getState());
+});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
